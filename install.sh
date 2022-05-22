52bd35e54c1f3edaa236fbe06e32934d084cae8e
Mustafa Yilmaz <mustafa.yilmaz@envanterium.com>
2022-05-22 12:36:17 +0300

    Completed homebrew based installations

--- install.sh
+++ install.sh
@@ -4,9 +4,16 @@ echo "Setting up dotfiles"
 
 if [ "$(uname)" == "Darwin" ]; then
 	echo "Running on Mac OS"
+	
+	read -p "Do you want to install brew and cask dependencies (recommended for clean setups): y/n/cancel" CONDITION;
 
-	echo ">> HOMEBREW"
-	source brew/brew.sh
+	if [ "$CONDITION" == "y" ]; then
+		echo ">> HOMEBREW"
+		source brew/brew.sh
+
+		echo ">> HOMEBREW CASK"
+		source brew/cask.sh
+	fi
 else
 	echo "Currently not configured for this system"
 	exit 0
