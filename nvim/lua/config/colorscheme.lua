72d4fdc4cb33a6cd4cd67010ee71d16e1ed75c16
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2022-11-22 12:23:55 +0300

    copilot reenabled

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,9 +1,9 @@
 vim.o.background = "dark" -- or "light" for light mode
 
 -- local colorscheme = "gruvbox-material"
-local colorscheme = "catppuccin"
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
--- local colorscheme = "tokyonight"
+-- local colorscheme = "catppuccin"
+-- vim.g.catppuccin_flavour = "latte" -- latte, frappe, macchiato, mocha
+local colorscheme = "tokyonight"
 -- local colorscheme = "gruvbox-baby"
 -- local colorscheme = 'neon'
 -- local colorscheme = 'codedark'
@@ -12,39 +12,39 @@ vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
-require("catppuccin").setup({
-	transparent_background = false,
-	term_colors = false,
-	compile = {
-		enabled = false,
-		path = vim.fn.stdpath("cache") .. "/catppuccin",
-	},
-	dim_inactive = {
-		enabled = false,
-		shade = "dark",
-		percentage = 0.15,
-	},
-	styles = {
-		comments = { "italic" },
-		conditionals = { "italic" },
-		loops = {},
-		functions = {},
-		keywords = {},
-		strings = {},
-		variables = {},
-		numbers = {},
-		booleans = {},
-		properties = {},
-		types = {},
-		operators = {},
-	},
-	integrations = {
-		-- For various plugins integrations see https://github.com/catppuccin/nvim#integrations
-	},
-	color_overrides = {},
-	highlight_overrides = {},
-})
-
+-- require("catppuccin").setup({
+-- 	transparent_background = false,
+-- 	term_colors = false,
+-- 	compile = {
+-- 		enabled = false,
+-- 		path = vim.fn.stdpath("cache") .. "/catppuccin",
+-- 	},
+-- 	dim_inactive = {
+-- 		enabled = false,
+-- 		shade = "dark",
+-- 		percentage = 0.15,
+-- 	},
+-- 	styles = {
+-- 		comments = { "italic" },
+-- 		conditionals = { "italic" },
+-- 		loops = {},
+-- 		functions = {},
+-- 		keywords = {},
+-- 		strings = {},
+-- 		variables = {},
+-- 		numbers = {},
+-- 		booleans = {},
+-- 		properties = {},
+-- 		types = {},
+-- 		operators = {},
+-- 	},
+-- 	integrations = {
+-- 		-- For various plugins integrations see https://github.com/catppuccin/nvim#integrations
+-- 	},
+-- 	color_overrides = {},
+-- 	highlight_overrides = {},
+-- })
+--
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found!")
 	return
