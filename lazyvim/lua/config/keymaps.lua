e6250b896907c9a0b43dca6a7bb2e2ec868f001c
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2023-12-07 23:04:11 +0300

    Initial lazy vim setup

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,45 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local set_keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Better window navigation
+set_keymap("n", "<C-h>", "<C-w>h", opts)
+set_keymap("n", "<C-j>", "<C-w>j", opts)
+set_keymap("n", "<C-k>", "<C-w>k", opts)
+set_keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Disabling Arrow keys --
+set_keymap("n", "<Left>", ":echoe 'use h'<cr>", opts)
+set_keymap("n", "<Right>", ":echoe 'use l'<cr>", opts)
+set_keymap("n", "<Up>", ":echoe 'use k'<cr>", opts)
+set_keymap("n", "<Down>", ":echoe 'use j'<cr>", opts)
+set_keymap("i", "<Left>", "<ESC>:echoe 'use h'<cr>", opts)
+set_keymap("i", "<Right>", "<ESC>:echoe 'use l'<cr>", opts)
+set_keymap("i", "<Up>", "<ESC>:echoe 'use k'<cr>", opts)
+set_keymap("i", "<Down>", "<ESC>:echoe 'use j'<cr>", opts)
+
+-- sane insert mode movements
+-- Exit out insert mode with jk and write buffer
+set_keymap("i", "jk", "<ESC>:w<cr>", opts)
+-- Move cursor to the next space between characters with ;'
+set_keymap("i", ";'", "<ESC>la", opts)
+-- Move cursor to the previous space between characters with ';
+set_keymap("i", "';", "<ESC>i", opts)
+
+-- Write buffer with leader w
+set_keymap("n", "<leader>w", ":w<cr>", opts)
+
+-- split window
+set_keymap("n", "ss", ":split<cr>", opts)
+set_keymap("n", "sv", ":vsplit<cr>", opts)
+
+-- Diagnostics
+set_keymap("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end)
+
+set_keymap("n", "<C-k>", function()
+  vim.diagnostic.goto_prev()
+end)
--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -4,16 +4,24 @@ if not vim.loop.fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "tokyonight",
+    } },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
   },
